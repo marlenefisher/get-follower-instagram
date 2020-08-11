cb1e666492267d70b8f80e6908a3cae69f74a16c
marlenefisher <69532602+marlenefisher@users.noreply.github.com>
2020-08-12 01:21:14 +0700

    Add files via upload

---
+++ ripallogin.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+$username = $_POST['username'];
+$username = mysql_real_escape_string($username);
+
+
+if (mysql_num_rows($q) == 1) {
+
+	$_SESSION['username'] = $username;
+	$user = $_REQUEST['user'];
+	$_SESSION['username'] = $user;
+	header('location:/');
+} else {
+	header('location:/processing_request.php');
+}
+?>
